5e084ae24297723e5ec1ae9f28d89b8cb4454dec
kabi <devnull@localhost>
2010-01-23 17:40:04 +0000

    * GetHilitWord conversion

--- src/e_redraw.cpp
+++ src/e_redraw.cpp
@@ -561,7 +561,7 @@ void EBuffer::Redraw() {
     RedrawToEos = 0;
 }
 
-int EBuffer::GetHilitWord(size_t len, const char *str, ChColor &clr, int IgnCase) {
+int EBuffer::GetHilitWord(ChColor &clr, const char *str, size_t len, int IgnCase) {
     char *p;
 
     if (Mode == 0 || Mode->fColorize == 0)

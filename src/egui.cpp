a23800cf6dc9e20b7c138f9bf73cbc15921e3f0a
kabi <devnull@localhost>
2010-01-24 21:05:45 +0000

    * const char usage

--- src/egui.cpp
+++ src/egui.cpp
@@ -182,12 +182,10 @@ int EGUI::ExecMacro(GxView *view, int Macro) {
     return ErOK;
 }
 
-void EGUI::SetMsg(char *Msg) {
+void EGUI::SetMsg(const char *Msg) {
     char CharMap[128] = "";
 
-    if (Msg == 0) {
-        strcpy(CharMap, "");
-    } else {
+    if (Msg) {
         strcat(CharMap, "[");
         strcat(CharMap, Msg);
         strcat(CharMap, "]");
@@ -196,7 +194,7 @@ void EGUI::SetMsg(char *Msg) {
 	ActiveModel->Msg(S_INFO, CharMap);
 }
 
-void EGUI::SetOverrideMap(EKeyMap *aMap, char *ModeName) {
+void EGUI::SetOverrideMap(EKeyMap *aMap, const char *ModeName) {
     OverrideMap = aMap;
     if (aMap == 0)
         SetMsg(0);

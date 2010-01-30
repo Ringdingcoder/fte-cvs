6ffc12b75ddd412b9ebc87a8d8be6a42f97c6b93
kabi <devnull@localhost>
2010-01-30 13:40:51 +0000

    * crlf converted

--- src/fnmatch.h
+++ src/fnmatch.h
@@ -16,8 +16,8 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 
-#ifndef	_FNMATCH_H
-#define	_FNMATCH_H	1
+#ifndef	FNMATCH_H
+#define	FNMATCH_H
 
 #ifdef	__cplusplus
 extern "C" {

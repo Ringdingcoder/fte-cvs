5b4f90b942b0d729604ef046560e7d57e412d548
kabi <devnull@localhost>
2010-02-20 10:55:02 +0000

    * internals of vector class are now all pointers

--- src/stl_fte.h
+++ src/stl_fte.h
@@ -79,7 +79,6 @@ public:
     static const size_type npos = ~0U;
 
     string() : str(empty_string) {}
-    string(char s);
     string(const char* s);
     string(const char* s, size_type len);
     string(const string& s);
@@ -121,6 +120,7 @@ public:
     void swap(string& s) { fte::swap(str, s.str); }
 
     /* string extensions */
+    string(char s);
     int sprintf(const char* fmt, ...) _fte_printf_attr(2, 3); // allocates size
     // it will use just 1024 bytes for non _GNU_SOURCE compilation!!
     string& tolower();
@@ -159,26 +159,24 @@ public:
     static const size_type invalid = ~0U;
 
     vector<Type>() :
-	m_type(0), m_capacity(0), m_size(0)
+	m_begin(0), m_capacity(m_begin), m_end(m_begin)
     {
     }
 
     vector<Type>(size_type prealloc) :
-	m_type(0), m_capacity(prealloc), m_size(0)
+	m_begin(prealloc ? new Type[prealloc] : 0),
+	m_capacity(m_begin + prealloc), m_end(m_begin)
     {
-	if (m_capacity > 0)
-	    m_type = new Type[m_capacity];
     }
 
     // we will not count references - we have to program with this in mind!
     vector<Type>(const vector<Type>& t) :
-	m_type(0), m_capacity(0), m_size(0)
+	m_begin(0), m_capacity(m_begin), m_end(m_begin)
     {
-	copy(t.m_type, t.m_size, t.m_size);
+	copy(t.m_begin, t.m_end, t.size());
     }
     vector<Type>& operator=(const vector<Type>& t)
     {
-	//printf("operator=    %p   %d	%d\n", t.m_type, t.m_size, t.m_capacity);
 	if (this != &t) {
 	    vector<Type> tmp(t);
 	    swap(tmp);
@@ -186,193 +184,176 @@ public:
 	return *this;
     }
     ~vector();
-    const_reference operator[](size_type i) const { return m_type[i]; }
-    reference operator[](size_type i) { return m_type[i]; }
-    const_iterator begin() const { return m_type; }
-    iterator begin() { return m_type; }
-    const_reference front() const { return *begin(); }
-    reference front() { return *begin(); }
-    const_iterator end() const { return m_type + m_size; }
-    iterator end() { return m_type + m_size; }
-    const_reference back() const { return *(end() - 1); }
-    reference back() { return *(end() - 1); }
-    size_type capacity() const { return m_capacity; }
-    void clear();
-    iterator erase(iterator pos);
+    const_reference operator[](size_type i) const { return m_begin[i]; }
+    reference operator[](size_type i) { return m_begin[i]; }
+    const_iterator begin() const { return m_begin; }
+    iterator begin() { return m_begin; }
+    const_reference front() const { return m_begin[0]; }
+    reference front() { return m_begin[0]; }
+    const_iterator end() const { return m_end; }
+    iterator end() { return m_end; }
+    const_reference back() const { return m_end[-1]; }
+    reference back() { return m_end[-1]; }
+    size_type capacity() const { return m_end - m_begin; }
+    void clear() { vector<Type> tmp; swap(tmp); }
+    bool empty() const { return (m_begin == m_end); }
+    iterator erase(iterator pos) { return erase(pos, pos + 1); }
     iterator erase(iterator first, iterator last);
-    iterator insert(iterator pos, const Type& t);
-    void insert(iterator pos, const_iterator from, const_iterator to);
-    size_type find(reference t) const
+    iterator insert(iterator pos, const Type& t)
     {
-	for (size_type i = 0; i < m_size; ++i)
-	    if (t == m_type[i])
-		return i;
-	return invalid;
+	const size_type n = pos - m_begin;
+	insert(pos, &t, &t + 1);
+	return m_begin + n;
     }
+    void insert(iterator pos, const_iterator from, const_iterator to);
     void pop_back()
     {
 	//printf("vector pop_back %d\n", m_size);
-	assert(m_size > 0);
-	if (--m_size < m_capacity / 4)
-	    internal_copy(m_type, m_size, m_capacity / 2);
+	assert(m_begin != m_end);
+        --m_end;
+	if (size() < capacity() / 4)
+	    internal_copy(m_begin, m_end, size() * 2);
     }
     void pop_front()
     {
-	assert(m_size > 0);
-	if (--m_size >= m_capacity / 4)
-	    for (size_type i = 1; i <= m_size; ++i)
-		internal_swap(m_type + (i - 1), m_type[i]);
+	assert(m_begin != m_end);
+        --m_end;
+	if (size() < capacity() / 4)
+	    internal_copy(m_begin + 1, m_end, size() * 2);
         else
-	    internal_copy(m_type + 1, m_size, m_capacity / 2);
+	    for (iterator it = m_begin; it != m_end; ++it)
+		internal_swap(it[0], it[1]);
     }
     void push_back(const_reference m)
     {
-	if (m_size + 1 >= m_capacity)
-	    internal_copy(m_type, m_size, m_capacity * 2);
-	m_type[m_size++] = m;
+	if (m_end == m_capacity)
+	    internal_copy(m_begin, m_end, capacity() * 2);
+	*m_end++ = m;
     }
     void reserve(size_type sz)
     {
-	if (sz > m_capacity)
-	    internal_copy(m_type, m_size, sz);
+	if (sz > capacity())
+	    internal_copy(m_begin, m_end, sz);
     }
     void resize(size_type sz)
     {
-	internal_copy(m_type, (sz < m_size) ? sz : m_size, sz);
-	m_size = sz;
+	internal_copy(m_begin, (sz < size()) ? m_begin + sz : m_end, sz);
+        m_end = m_begin + sz; // even empty members
     }
-    size_type size() const { return m_size; }
+    size_type size() const { return m_end - m_begin; }
     void swap(vector<Type>& v)
     {
-	fte::swap(m_type, v.m_type);
+	fte::swap(m_begin, v.m_begin);
 	fte::swap(m_capacity, v.m_capacity);
-	fte::swap(m_size, v.m_size);
+	fte::swap(m_end, v.m_end);
     }
 
     /* vector extensions */
+    size_type find(reference t) const
+    {
+	for (iterator it = m_begin; it != m_end; ++it)
+	    if (t == *it)
+		return (it - m_begin);
+	return invalid;
+    }
     void remove(const_reference t);
 
 protected:
     static const size_type min_capacity = 4;
-    Type* m_type;
-    size_type m_capacity;
-    size_type m_size;
-    void copy(const_iterator in, size_type size, size_type alloc);
-    void internal_copy(iterator in, size_type size, size_type alloc);
-    void internal_swap(iterator pos, Type& t) { *pos = t; }
+    Type* m_begin;
+    Type* m_capacity;
+    Type* m_end;
+    void copy(const_iterator first, const_iterator last, size_type alloc);
+    void internal_copy(iterator first, iterator last, size_type alloc);
+    void internal_swap(Type& a, Type& b) { a = b; }
 };
 
 template <class Type>
 vector<Type>::~vector()
 {
-    delete[] m_type;
+    delete[] m_begin;
 }
 
 template <class Type>
-void vector<Type>::clear()
+void vector<Type>::copy(const_iterator first, const_iterator last, size_type alloc)
 {
-    delete[] m_type;
-    m_type = 0;
-    m_capacity = 0;
-    m_size = 0;
-}
-
-template <class Type>
-void vector<Type>::copy(const_iterator in, size_type sz, size_type alloc)
-{
-    assert(sz <= alloc);
+    assert(size() <= alloc);
     vector<Type> tmp((alloc < min_capacity) ? min_capacity : alloc);
-    for (size_type i = 0; i < sz; ++i)
-	tmp[i] = in[i];
+    for (; first != last; ++tmp.m_end, ++first)
+	*tmp.m_end = *first;
     swap(tmp);
-    m_size = sz;
 }
 
 template <class Type>
-void vector<Type>::internal_copy(iterator in, size_type sz, size_type alloc)
+void vector<Type>::internal_copy(iterator first, iterator last, size_type alloc)
 {
-    assert(sz <= alloc);
+    assert(size() <= alloc);
     vector<Type> tmp((alloc < min_capacity) ? min_capacity : alloc);
-    for (size_type i = 0; i < sz; ++i)
-	internal_swap(tmp.begin() + i, in[i]);
+    for (; first != last; ++tmp.m_end, ++first)
+	internal_swap(*tmp.m_end, *first);
     swap(tmp);
-    m_size = sz;
-}
-
-template <class Type>
-typename vector<Type>::iterator vector<Type>::erase(iterator pos)
-{
-    return erase(pos, pos + 1);
 }
 
 template <class Type>
 typename vector<Type>::iterator vector<Type>::erase(iterator first, iterator last)
 {
-    assert(last <= end());
-    m_size -= last - first;
-    for (iterator p = first; p < end(); ++last, ++p)
-	internal_swap(p, *last);
+    assert(last <= m_end);
+    m_end -= (last - first);
+    for (iterator it = first; it != m_end; ++last, ++it)
+	internal_swap(*it, *last);
     return first;
-}
-
-template <class Type>
-typename vector<Type>::iterator vector<Type>::insert(iterator pos, const Type& t)
-{
-    const size_type n = pos - begin();
-    insert(pos, &t, &t + 1);
-    return begin() + n;
 }
 
 template <class Type>
 void vector<Type>::insert(iterator pos, const_iterator from, const_iterator to)
 {
-    const size_type isz = to - from;
+    size_type isz = to - from;
     if (!isz)
 	return;
 
-    const size_type n = pos - begin();
     Type* tmp;
 
-    if (m_size + isz < m_capacity)
-	tmp = begin();
+    if (m_end + isz < m_capacity)
+	tmp = m_begin;
     else
     {
-	const size_type nc = ((m_size + isz > min_capacity)
-			      ? m_size + isz : min_capacity) * 2;
+	size_type nc = ((size() + isz > min_capacity)
+			? size() + isz : min_capacity) * 2;
 	tmp = new Type[nc];
-	m_capacity = nc;
+	m_capacity = tmp + nc;
     }
 
-    // works also for m_size == 0
-    for (size_type i = m_size - 1; i + 1 >= n + isz; --i)
-	internal_swap(tmp + (i + isz), m_type[i]);
+    iterator l = m_end;
+    m_end = tmp + size() + isz;
+    if (l)
+	for (iterator it = m_end; --l >= pos;)
+	    internal_swap(*--it, *l);
 
-    if (tmp != m_type)
+    size_type n = pos - m_begin;
+    for (iterator it = tmp + n; from != to; ++it, ++from)
+	*it = *from;
+
+    if (tmp != m_begin)
     {
 	for (size_type i = 0; i < n; ++i)
-	    internal_swap(tmp + i, m_type[i]);
-	delete[] m_type;
-	m_type = tmp;
+	    internal_swap(tmp[i], m_begin[i]);
+	delete[] m_begin;
+	m_begin = tmp;
     }
-
-    for (size_type i = n; from != to; ++i, ++from)
-	m_type[i] = *from;
-
-    m_size += isz;
 }
 
 template <class Type>
 void vector<Type>::remove(const_reference t)
 {
-    iterator from = begin();
-    for (iterator it = from; it != end(); ++it)
+    iterator from = m_begin;
+    for (iterator it = from; it != m_end; ++it)
 	if (t != *it)
 	{
 	    if (from != it)
-		internal_swap(from, *it);
+		internal_swap(*from, *it);
 	    ++from;
 	}
-    m_size -= (end() - from);
+    m_end = from;
 }
 
 template <class Type>
@@ -381,12 +362,12 @@ inline void swap(fte::vector<Type>& a, fte::vector<Type>& b) { a.swap(b); }
 /*
  * partial specialization for some common types
  * for more effective transfers in copy constructions
- * instead of heaving two copies - we allow to swap between field values
+ * instead of having two copies - we allow to swap between field values
  * thus we can maintain vector<string> without reallocation of string with
  * every size change of vector<>
  */
 template <>
-inline void vector<fte::string>::internal_swap(vector<fte::string>::iterator pos, fte::string& t) { (*pos).swap(t); }
+inline void vector<fte::string>::internal_swap(fte::string& a, fte::string& b) { a.swap(b); }
 
 
 #define StlString   fte::string
